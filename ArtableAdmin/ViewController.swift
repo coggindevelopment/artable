4bb291aa54e4e92aa173fc30a0c34c674b866665
Nicholas Coggin <coggindevelopment@gmail.com>
2019-10-29 05:38:31 -0500

    Initial commit

---
+++ ArtableAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtableAdmin
+//
+//  Created by Nicholas Coggin on 10/29/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
